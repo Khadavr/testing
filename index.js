6bc72423427ee27e1f5972ac80a152f6ff04d797
Khadavr <incoffin@gmail.com>
2021-05-05 16:56:09 +0300

    add index.html, main.css; modify index.js

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-import _ from "lodash";
-console.log(_.last(["one", "two"]));
+const model = [{
+  type: 'title',
+  value: 'Hello Creator'
+}, {
+  type: 'text',
+  value: 'some good text'
+}, {
+  type: 'columns',
+  value: [
+    '11111111',
+    '22222222',
+    '333333333'
+  ]
+}];
+
+const $site = document.querySelector('#site');
+
+model.forEach(block => {
+  let html = '';
+
+  if (block.type === 'title') {
+    html = title(block);
+  } else if (block.type === 'text') {
+    html = ;
+  } else if (block.type === 'columns') {
+      html = 
+      
+      
+  }
+
+  $site.insertAdjacentHTML("beforeend", html);
+}
+
+function title(block) {
+    return `<div class="row">
+    <div class="col-sm">
+      <h1>${block.value}</h1>
+     </div>
+  </div>`
+}
 
