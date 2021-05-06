d6b1aac0217871b35f92b6ef13cba5a470b4fd4a
Khadavr <incoffin@gmail.com>
2021-05-06 15:34:09 +0300

    change: minor changes

---
+++ src/model.js
@@ -0,0 +1,17 @@
+export const model = [{
+  type: 'title',
+  value: 'Hello Creator!!!!!!',
+}, {
+  type: 'text',
+  value: 'some good text',
+}, {
+  type: 'columns',
+  value: [
+    '11111111',
+    '22222222',
+    '333333333',
+  ],
+}, {
+  type: 'image',
+  value: './assets/image.png',
+}];
